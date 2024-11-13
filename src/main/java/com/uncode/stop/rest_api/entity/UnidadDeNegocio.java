49b4922eebf990f16bbeee0d91d84e2b24a8dbff
VictorRamirez26 <victorriver6@hotmail.com>
2024-11-13 18:34:26 -0300

    Agrego realcion de servicio a Unidad de negocio

--- src/main/java/com/uncode/stop/rest_api/entity/UnidadDeNegocio.java
+++ src/main/java/com/uncode/stop/rest_api/entity/UnidadDeNegocio.java
@@ -1,6 +1,5 @@
 package com.uncode.stop.rest_api.entity;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.SoftDelete;
@@ -10,12 +9,10 @@ import com.uncode.stop.rest_api.service.Identifiable;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -34,7 +31,7 @@ public class UnidadDeNegocio implements Identifiable<UUID>{
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Direccion direccion;
     
-    @OneToMany(fetch = FetchType.EAGER)
-    private List<Servicio> servicio;
+    @ManyToOne
+    private Servicio servicio;
 
 }
