816051486515d8eeaa6bd9c93bce892ddc0555a4
adrzanbar <adrzanbar@gmail.com>
2024-11-14 11:13:33 -0300

    idk what this is

--- src/main/java/com/uncode/stop/rest_api/controller/HabitanteController.java
+++ src/main/java/com/uncode/stop/rest_api/controller/HabitanteController.java
@@ -1,10 +1,15 @@
 package com.uncode.stop.rest_api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.uncode.stop.rest_api.dto.HabitanteDTO;
 import com.uncode.stop.rest_api.entity.Habitante;
 import com.uncode.stop.rest_api.service.HabitanteService;
 import com.uncode.stop.rest_api.service.PersonaService;
 
+@RestController
+@RequestMapping("/habitantes")
 public class HabitanteController extends PersonaController<Habitante, HabitanteDTO> {
 
     public HabitanteController(HabitanteService service, PersonaService personaService) {
